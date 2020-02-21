836c71c4c15a87affbe42ac8cdb94664847230f7
Hailey Bongo <haileyebongo@gmail.com>
2020-02-21 18:20:19 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,10 @@
 class Project
   
-  attr_reader :name
+  attr_reader :title
+  attr_accessor :backers
   
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @backers = []
   end
   
